d6ef145d21eecbef9fce3d13b91ff23081b85485
feiyanqiqiao <zhangqiao33@gmail.com>
2025-07-23 12:41:45 +0800

    Add preview data and UI tests

--- myBiographyTests/myBiographyTests.swift
+++ myBiographyTests/myBiographyTests.swift
@@ -5,13 +5,19 @@
 //  Created by zhangqiao on 2025/7/16.
 //
 
+import CoreData
 import Testing
 @testable import myBiography
 
 struct myBiographyTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @MainActor
+    @Test func testPreviewSeedData() async throws {
+        let preview = PersistenceController.preview
+        let context = preview.container.viewContext
+        let fetch = NSFetchRequest<NSManagedObject>(entityName: "Item")
+        let items = try context.fetch(fetch)
+        #expect(items.count == 10)
     }
 
 }
